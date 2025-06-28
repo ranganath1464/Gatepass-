d3482feace562be55681ed262989123ab37ef501
Gajula Ranganath <ranga1464prince@gmail.com>
2025-06-28 21:48:34 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -4,7 +4,7 @@ import psycopg2.extras
 
 def get_connection():
     return psycopg2.connect(
-        host="ep-noisy-sea-a5htqu3p-pooler.us-east-2.aws.neon.tech",
+        host="ep-orange-lab-a5vjihxa-pooler.us-east-2.aws.neon.tech",
         database="neondb",
         user="neondb_owner",
         password="npg_MN8fpYk0qDKr",
