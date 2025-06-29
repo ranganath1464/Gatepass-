fab0d66db851f524acd63924d93ac8064a0b6845
Gajula Ranganath <ranga1464prince@gmail.com>
2025-06-29 14:34:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session, f
 from db_config import get_connection
 import psycopg2.extras
 from datetime import datetime
+import pytz
 import smtplib, ssl
 import random
 import os
@@ -148,7 +149,7 @@ def student_gatepass():
 
     if request.method == 'POST':
         reason = request.form['reason']
-        request_date = datetime.now()
+        request_date = datetime.now(pytz.timezone("Asia/Kolkata"))
 
         cur.execute("""
             INSERT INTO gatepass_requests (student_id, reason, status, request_date)
@@ -242,11 +243,12 @@ def faculty_approve(req_id):
         """, (status, remark, req_id))
         conn.commit()
 
+        ist_time = data['request_date'].astimezone(pytz.timezone("Asia/Kolkata"))
         subject = f"Gatepass Request {status.capitalize()} - Gatepass System"
         body = f"""
 Dear {data['name']},
 
-Your gatepass request submitted on {data['request_date'].strftime('%Y-%m-%d %H:%M')} has been {status.upper()}.
+Your gatepass request submitted on {ist_time.strftime('%Y-%m-%d %H:%M')} has been {status.upper()}.
 
 Faculty Remark: {remark}
 
@@ -273,4 +275,4 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(debug=True)  
+    app.run(debug=True)
